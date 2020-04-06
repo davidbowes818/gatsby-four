6bb11e3377c02ec98ca429521ae568161349a687
iloveket <davidrb2017@outlook.com>
2020-04-06 19:03:33 +0100

    blog template

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,3 +1,4 @@
+const path = require("path")
 const { createFilePath } = require("gatsby-source-filesystem")
 
 exports.onCreateNode = ({ node, getNode, actions }) => {
@@ -10,6 +11,8 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 }
 
 exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+
   const result = await graphql(`
     query {
       allMarkdownRemark {
@@ -23,5 +26,14 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  console.log(JSON.stringify(result, null, 4))
+
+  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: path.resolve("./src/templates/blog-post.js"),
+      context: {
+        slug: node.fields.slug,
+      },
+    })
+  })
 }
